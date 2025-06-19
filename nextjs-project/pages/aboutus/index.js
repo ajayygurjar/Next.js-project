eadd6ea162c116e52c04fe2d2606ed46bdc86998
ajayygurjar <167853061+ajayygurjar@users.noreply.github.com>
2025-06-19 15:11:12 +0530

    Final touch to About US page adding list in about page

--- nextjs-project/pages/aboutus/index.js
+++ nextjs-project/pages/aboutus/index.js
@@ -1,4 +1,26 @@
+import Link from "next/link";
+
+
+const details = [
+  { id: '1', name: 'Yash', role: 'Senior Developer' },
+  { id: '2', name: 'Vaibhav', role: 'Backend Developer' },
+  { id: '3', name: 'Suresh', role: 'Frontend Developer' }
+];
+ 
  function AboutUs() {
-  return <h1>The AboutUs Page</h1>;
+  return(
+  <div>
+      <h1>Our Team</h1>
+      <ul>
+        {details.map((dev) => (
+          <li key={dev.id}>
+            <Link href={`/aboutus/${dev.id}`}>
+              {dev.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+    );
 }
 export default AboutUs;
